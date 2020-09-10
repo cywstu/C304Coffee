eb5cf05710a025df8ccc8ec398689a2d16b734d2
cywstu <46858924+cywstu@users.noreply.github.com>
2020-09-10 22:12:45 +0800

    basic done
    not tested but seems fine?
    since this api part is kinda ready, am gonna move to client
    targets remain:
    1. autotests
    2. ci/cd
    3. cleaning up the codes

--- routes/router.js
+++ routes/router.js
@@ -2,6 +2,10 @@ const express = require("express");
 const router = express.Router();
 const Coffee = require("../models/Coffee");
 
+//=============================================
+// coffee
+//=============================================
+
 router.get("/", (req, res) => {
     res.send("hi");
 });
@@ -17,12 +21,17 @@ router.get("/coffee", async (req, res) => {
 
 router.get("/coffee/:coffeeId", async (req, res) => {
     try{
-        const coffee = await Post.find();
-        res.json(posts);
+        const curCoffee = await Coffee.findById(req.params.coffeeId);
+        res.json(curCoffee);
     }catch(err){
         res.json({message:err});
     }
 });
+
+//=============================================
+// login/logout
+//=============================================
+
 
 router.get("/login", (req, res) => {
     try{
@@ -45,6 +54,33 @@ router.post("/login", async (req, res) => {
         res.json({message: err});
     }
     console.log("login post");
+});
+
+//=============================================
+// add/delete coffee
+//=============================================
+
+router.delete("/deleteCoffee/:coffeeId", async(req,res) =>{
+    try{
+        const removedPost = await Coffee.remove({_id: req.params.postId});
+        res.json(removedPost);
+    }catch(err){
+        res.json({ message: err});
+    }    
+});
+
+router.patch("/updateCoffee/:coffeeId", async(req,res) =>{
+    try{
+        const updatedCoffee = await Coffee.update(
+            {_id: req.params.postId},
+            {$set:{
+                name: req.body.name,
+                desc:req.body.desc
+            }});
+        res.json(updatedCoffee);
+    }catch(err){
+        res.json({ message: err});
+    }    
 });
 
 router.post("/addcoffee", async (req, res) => {
