c91b7a484f2513161a06557dba23afb017db9c06
cywstu <46858924+cywstu@users.noreply.github.com>
2020-09-15 01:19:37 +0800

    full cop of the code

--- app.js
+++ app.js
@@ -10,10 +10,10 @@ app.use(bodyParser.json());
 //cors problem
 app.use((req, res, next) =>{
     res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Headers", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
     if(req.method === "OPTIONS"){
-        res.header("Access-Control-Allow-Methods", "GET, PUT, POST, PUT, PATCH, DELETE");
-        res.status(200).json({});
+        res.header("Access-Control-Allow-Methods", "GET, POST, PUT, PATCH, DELETE");
+        return res.status(200).json({});
     }
     next();
 });
