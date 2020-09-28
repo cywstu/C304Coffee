2c0a1912eb203abb1286360a715cb56538c65a67
cywstu <46858924+cywstu@users.noreply.github.com>
2020-09-28 16:26:12 +0800

    finish
    document not doing very well but hey at least its done
    now just have to try deploying on heroku

--- routes/router.js
+++ routes/router.js
@@ -37,21 +37,17 @@ router.get("/", (req, res) => {
  *    produces:
  *      - application/json
  *    parameters:
- *      - name: username
- *        description: username
+ *      - name: User
+ *        description: object of user
  *        in: body
  *        required: true
- *        type: string
- *      - name: password
- *        description: password.
- *        in: body
- *        required: true
- *        type: string
+ *        schema:
+ *          $ref: "#/definitions/User"
  *    responses:
  *      "2xx":
  *        description: successful response
  *        schema:
- *          $ref: '#/definitions/NewUser'
+ *          $ref: "#/definitions/TokenMessage"
  */
 router.post("/login", Controller.login);
 
@@ -60,38 +56,137 @@ router.post("/login", Controller.login);
  * /coffees:
  *  get:
  *    description: request all types of coffee
+ *    produces:
+ *      - application/json
  *    responses:
  *      "2xx":
  *        description: successful response
- *        content:
- *        application/json:
- *          schema:
- *            type: Coffee (models/Coffee.js)
- *            properties:
- *              _id:
- *                type: string
- *                example: q956kgnsdq3
- *              name:
- *                type: string
- *                example: simple coffee
- *              desc:
- *                type: string
- *                example: this is simple coffee
- *              addDate:
- *                type: date
- *                example: 1945-08-24T19:45:08.242Z
- *              image:
- *                type: buffer
+ *        schema:
+ *          type: array
+ *          items:
+ *            $ref: "#/definitions/Coffee"
  */
 router.get("/coffees", Controller.getAllCoffees);
-router.get("/coffees/:coffeeName", Controller.getCoffee);
-router.get("/coffees/:coffeeName/image", Controller.getCoffeeImage);
-router.get("/coffees/id/:coffeeId", Controller.getCoffeeFromId);
 
-//take {name: ?string, desc: ?string, image: ?buffer}
+ /**
+ * @swagger
+ * /coffees/:coffeeName:
+ *  get:
+ *    description: request a single coffee via name
+ *    produces:
+ *      - application/json
+ *    responses:
+ *      "2xx":
+ *        description: successful response
+ *        schema:
+ *          $ref: "#/definitions/Coffee"
+ */
+router.get("/coffees/:coffeeName", Controller.getCoffee);
+
+/**
+ * @swagger
+ * /coffees/:coffeeName/image:
+ *  get:
+ *    description: request the image of a coffee
+ *    produces:
+ *      - application/json
+ *    responses:
+ *      "2xx":
+ *        description: successful response
+ *        schema:
+ *          $ref: "#/definitions/CoffeeImage"
+ */
+router.get("/coffees/:coffeeName/image", Controller.getCoffeeImage);
+//router.get("/coffees/id/:coffeeId", Controller.getCoffeeFromId);
+
+/**
+ * @swagger
+ * /add:
+ *  post:
+ *    description: adding a coffee
+ *    produces:
+ *      - application/json
+ *    parameters:
+ *      - name: token
+ *        description: a valid token from login
+ *        in: Headers/Authorization
+ *        required: true
+ *      - name: Coffee
+ *        description: object of a coffee
+ *        in: body
+ *        required: true
+ *        schema:
+ *          $ref: "#/definitions/Coffee"
+ *      - name: CoffeeImage
+ *        description: image of a coffee
+ *        in: form
+ *        required: true
+ *        schema:
+ *          $ref: "#/definitions/CoffeeImage"
+ *    responses:
+ *      "2xx":
+ *        description: successful response
+ *        schema:
+ *          $ref: "#/definitions/AddMessage"
+ */
 router.post("/add", auth, upload.single("image"), Controller.addCoffee);
-//take {name: ?string, desc: ?string, image: ?buffer}
+
+/**
+ * @swagger
+ * /edit/:coffeeId:
+ *  put:
+ *    description: editing a coffee
+ *    produces:
+ *      - application/json
+ *    parameters:
+ *      - name: token
+ *        description: a valid token from login
+ *        in: Headers/Authorization
+ *        required: true
+ *      - name: Coffee
+ *        description: object of a coffee
+ *        in: body
+ *        required: true
+ *        schema:
+ *          $ref: "#/definitions/Coffee"
+ *      - name: CoffeeImage
+ *        description: image of a coffee
+ *        in: form
+ *        required: true
+ *        schema:
+ *          $ref: "#/definitions/CoffeeImage"
+ *    responses:
+ *      "2xx":
+ *        description: successful response
+ *        schema:
+ *          $ref: "#/definitions/Message"
+ */
 router.put("/edit/:coffeeId", auth, upload.single("image"), Controller.editCoffee);
+
+/**
+ * @swagger
+ * /remove/:coffeeId:
+ *  delete:
+ *    description: deleting a coffee
+ *    produces:
+ *      - application/json
+ *    parameters:
+ *      - name: token
+ *        description: a valid token from login
+ *        in: Headers/Authorization
+ *        required: true
+ *      - name: id
+ *        description: id of the coffee
+ *        in: body
+ *        required: true
+ *        type: string
+ *        example: "5f70ff5b5cddac1e8c389414"
+ *    responses:
+ *      "2xx":
+ *        description: successful response
+ *        schema:
+ *          $ref: "#/definitions/Message"
+ */
 router.delete("/remove/:coffeeId", auth, Controller.removeCoffee);
 
 //================================================
@@ -102,26 +197,107 @@ router.delete("/remove/:coffeeId", auth, Controller.removeCoffee);
  * @swagger
  *
  * definitions:
- *   NewUser:
+ *   Message:
  *     type: object
- *     required:
- *       - username
- *       - password
+ *     properties:
+ *       message:
+ *         description: telling if api request succeed or failed
+ *         type: string
+ *         required: true
+ *         example: "adding coffee success!"
+ */
+
+ /**
+ * @swagger
+ *
+ * definitions:
+ *   TokenMessage:
+ *     type: object
+ *     properties:
+ *       message:
+ *         description: telling if api request succeed or failed
+ *         type: string
+ *         required: true
+ *         example: "login success!"
+ *       token:
+ *         description: will be given on login success, for validating the ap request was sent from approved user
+ *         type: string
+ *         required: true
+ *         example: "df458mnMkoa7hW87sU56nnb7hsnkjaF"   
+ */
+
+ /**
+ * @swagger
+ *
+ * definitions:
+ *   AddMessage:
+ *     type: object
+ *     properties:
+ *       message:
+ *         description: telling if api request succeed or failed
+ *         type: string
+ *         required: true
+ *         example: "coffee added!"
+ *       id:
+ *         description: will be given on adding coffee success
+ *         type: string
+ *         required: true
+ *         example: "5f70ff5b5cddac1e8c389414"   
+ */
+
+/**
+ * @swagger
+ *
+ * definitions:
+ *   User:
+ *     type: object
  *     properties:
  *       username:
+ *         description: username
  *         type: string
+ *         required: true
+ *         example: "username"
  *       password:
+ *         description: password
  *         type: string
- *         format: password
- *   User:
- *     allOf:
- *       - $ref: '#/definitions/NewUser'
- *       - required:
- *         - id
- *       - properties:
- *         id:
- *           type: integer
- *           format: int64
+ *         required: true
+ *         example: "password"
+ */
+
+ /**
+ * @swagger
+ *
+ * definitions:
+ *   Coffee:
+ *     type: object
+ *     properties:
+ *       name:
+ *         description: name of the coffee
+ *         type: string
+ *         required: true
+ *         example: "simple coffee"
+ *       desc:
+ *         description: description of the coffee
+ *         type: string
+ *         required: true
+ *         example: "this is simple coffee"
+ *       addDate:
+ *         description: the date of coffee being added, don't need to add this for request
+ *         type: date
+ *         example: 1945-02-08T19:45:02.080Z
+ */
+
+/**
+ * @swagger
+ *
+ * definitions:
+ *   CoffeeImage:
+ *     type: buffer
+ *     properties:
+ *       image:
+ *         description: image buffer of the coffee
+ *         type: buffer
+ *         required: true
  */
 
 module.exports = router;
