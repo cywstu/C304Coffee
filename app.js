6ff0dc7e2a995a3ffcdb0c431b4c21cd4123b9c1
cywstu <46858924+cywstu@users.noreply.github.com>
2020-09-28 23:03:39 +0800

    try adding cors header
    angular failed to request, just try

--- app.js
+++ app.js
@@ -9,18 +9,18 @@ app.use(morgan("dev"));
 const bodyParser = require("body-parser");
 app.use(bodyParser.json());
 
-/*
+
 //cors problem
 app.use((req, res, next) =>{
     res.header("Access-Control-Allow-Origin", "*");
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
     if(req.method === "OPTIONS"){
-        res.header("Access-Control-Allow-Methods", "GET, POST, PUT, PATCH, DELETE");
+        res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
         return res.status(200).json({});
     }
     next();
 });
-*/
+
 
 //custom modules
 const router = require("./routes/router");
