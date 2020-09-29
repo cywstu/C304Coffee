77f2032c3c4b861425de8f0fc4eb0d4ce2056942
cywstu <46858924+cywstu@users.noreply.github.com>
2020-09-29 22:06:47 +0800

    Revert "try modifying header"
    This reverts commit c629f5f07ccdf9f97dbcb964ad7f58a6337da8e5.

--- app.js
+++ app.js
@@ -13,9 +13,11 @@ app.use(bodyParser.json());
 //cors problem
 app.use((req, res, next) =>{
     res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
-    res.setHeader('Access-Control-Allow-Credentials', true);
+    if(req.method === "OPTIONS"){
+        res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
+        return res.status(200).json({});
+    }
     next();
 });
 
