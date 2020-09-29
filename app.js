c629f5f07ccdf9f97dbcb964ad7f58a6337da8e5
cywstu <46858924+cywstu@users.noreply.github.com>
2020-09-29 22:02:52 +0800

    try modifying header

--- app.js
+++ app.js
@@ -13,11 +13,9 @@ app.use(bodyParser.json());
 //cors problem
 app.use((req, res, next) =>{
     res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
-    if(req.method === "OPTIONS"){
-        res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
-        return res.status(200).json({});
-    }
+    res.setHeader('Access-Control-Allow-Credentials', true);
     next();
 });
 
